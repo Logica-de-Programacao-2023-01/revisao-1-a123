40f2ead8d4133d1917caef1032909f1abe039807
miltonpedro <126252785+miltonpedro@users.noreply.github.com>
2023-06-29 22:17:48 -0300

    Update q2.go

--- q2/q2.go
+++ q2/q2.go
@@ -1,6 +1,23 @@
-package q2
+package main
 
-func AverageLettersPerWord(text string) (float64, error) {
-	// Implemente sua solução aqui
-	return 0, nil
+import (
+	"errors"
+	"strings"
+)
+
+func calcularMediaLetrasPorPalavra(texto string) (float64, error) {
+	palavras := strings.Fields(texto)
+	totalPalavras := len(palavras)
+	totalLetras := 0
+
+	if totalPalavras == 0 {
+		return 0, errors.New("texto vazio")
+	}
+
+	for _, palavra := range palavras {
+		totalLetras += len(palavra)
+	}
+
+	media := float64(totalLetras) / float64(totalPalavras)
+	return media, nil
 }
